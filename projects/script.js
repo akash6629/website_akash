bddcecc37f4650cadcf0e93eaf7c70bdbf63603e
Akash <akashmangond6656@gmail.com>
2025-09-13 23:40:37 +0530

    akash

---
+++ projects/script.js
@@ -0,0 +1,188 @@
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Projects | Portfolio ";
+            $("#favicon").attr("href", "/assets/images/favicon.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "/assets/images/favhand.png");
+        }
+    });
+
+
+// fetch projects start
+function getProjects() {
+    return fetch("projects.json")
+        .then(response => response.json())
+        .then(data => {
+            return data
+        });
+}
+
+
+function showProjects(projects) {
+    let projectsContainer = document.querySelector(".work .box-container");
+    let projectsHTML = "";
+    projects.forEach(project => {
+        projectsHTML += `
+        <div class="grid-item ${project.category}">
+        <div class="box tilt" style="width: 380px; margin: 1rem">
+      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`
+    });
+    projectsContainer.innerHTML = projectsHTML;
+
+    // vanilla tilt.js
+    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    //     max: 20,
+    // });
+    // // vanilla tilt.js  
+
+    // /* ===== SCROLL REVEAL ANIMATION ===== */
+    // const srtop = ScrollReveal({
+    //     origin: 'bottom',
+    //     distance: '80px',
+    //     duration: 1000,
+    //     reset: true
+    // });
+
+    // /* SCROLL PROJECTS */
+    // srtop.reveal('.work .box', { interval: 200 });
+
+    // isotope filter products
+    var $grid = $('.box-container').isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        masonry: {
+            columnWidth: 200
+        }
+    });
+
+    // filter items on button click
+    $('.button-group').on('click', 'button', function () {
+        $('.button-group').find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+}
+
+getProjects().then(data => {
+    showProjects(data);
+})
+// fetch projects end
+
+// Start of Tawk.to Live Chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+// End of Tawk.to Live Chat
+
+// disable developer mode
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+const firebaseConfig = {
+    apiKey: "AIzaSyCyN7jNXkjpAMSri2PWivTa6kLwib7h4Qo",
+    authDomain: "contact-dd543.firebaseapp.com",
+    databaseURL: "https://contact-dd543-default-rtdb.firebaseio.com",
+    projectId: "contact-dd543",
+    storageBucket: "contact-dd543.appspot.com",
+    messagingSenderId: "363620136330",
+    appId: "1:363620136330:web:38433d65f81e4360884473"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  var contactFormDB = firebase.database().ref("contact-form");
+
+document.getElementById("contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var emailid = getElementVal("email");
+  var message = getElementVal("message");
+  var phone = getElementVal("phone");
+
+  saveMessages(name, emailid, message, phone );
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+
+  //   reset the form
+  document.getElementById("contact-form").reset();
+}
+
+const saveMessages = (name, emailid, msgContent,phone) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    name: name,
+    emailid: emailid,
+    msgContent: msgContent,
+    phone:phone
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
